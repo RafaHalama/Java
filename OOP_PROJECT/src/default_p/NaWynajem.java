159f7c1f2658ea133629532eacc98254752408bb
Ryu <rafau.halama@gmail.com>
2023-05-18 11:47:47 +0200

    del

--- OOP_PROJECT/src/default_p/NaWynajem.java
+++
@@ -1,9 +0,0 @@
-package default_p;
-
-import java.util.List;
-
-public interface NaWynajem {
-	
-	public List<Objekt> getLista();
-
-}
--- OOP_PROJECT/src/default_p/NeverRentException.java
+++
@@ -1,6 +0,0 @@
-package default_p;
-
-public class NeverRentException extends Exception {
-	public NeverRentException(){
-super("Ta osoba nigdy nic nie wynajela");}
-}
